a2497423fde09f14e92cfee7c4986d4493011a0b
Ester Castellano Ríos <estercastellanorios@gmail.com>
2026-02-27 16:34:41 +0100

    [Vistas]: Se ha implementado en la vista kanban que se puean crear sprints y tareas

--- volumesOdoo/addons/scrum_ecr/models/sprint.py
+++ volumesOdoo/addons/scrum_ecr/models/sprint.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import timedelta
 
 class Sprint(models.Model):
 	_name = 'scrum_ecr.sprint'
@@ -8,8 +9,8 @@ class Sprint(models.Model):
 
 	name_ecr = fields.Char(string='Nombre', required=True)
 	description_ecr = fields.Text(string='Descripción')
-	date_start_ecr = fields.Date(string='Fecha de inicio', required=True)
-	date_end_ecr = fields.Date(string='Fecha de finalización', required=True)
+	date_start_ecr = fields.Date(string='Fecha de inicio', required=True, default=fields.Date.today)
+	date_end_ecr = fields.Date(string='Fecha de finalización', required=True, default=lambda self: fields.Date.today() + timedelta(days=14))
 	task_ids_ecr = fields.One2many(
         comodel_name='scrum_ecr.task',
         inverse_name='sprint_id_ecr',
